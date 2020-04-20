43feaefde8b83c57935d5d212e3c4c9c7f56fb94
Lohen Yumnam <lohensana@gmail.com>
2020-04-20 14:59:48 +0530

    Updates

---
+++ precache-manifest.63c1cff18dfdb2cf4e83b73d2a93b5c6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4588eb6e7c9935231026a18c26aa542",
+    "url": "/reactGitPage/index.html"
+  },
+  {
+    "revision": "31486e42f660022e4ce9",
+    "url": "/reactGitPage/static/css/main.cecf8dc3.chunk.css"
+  },
+  {
+    "revision": "a9071ad9ddfe26fee23c",
+    "url": "/reactGitPage/static/js/2.87aa1730.chunk.js"
+  },
+  {
+    "revision": "9b707ee842af4f317f626a3c3900dd73",
+    "url": "/reactGitPage/static/js/2.87aa1730.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31486e42f660022e4ce9",
+    "url": "/reactGitPage/static/js/main.bb3b5e59.chunk.js"
+  },
+  {
+    "revision": "dc192b57bfed2afd6be1ea5558f17255",
+    "url": "/reactGitPage/static/js/main.bb3b5e59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc42344740d2011b73db",
+    "url": "/reactGitPage/static/js/runtime-main.53f4b39d.js"
+  },
+  {
+    "revision": "bd6820f1a4015e9645e470ea8e7b1501",
+    "url": "/reactGitPage/static/media/anime3.bd6820f1.png"
+  },
+  {
+    "revision": "9fcf69e5359987c9b1545aae1c2c5faa",
+    "url": "/reactGitPage/static/media/emilyz.9fcf69e5.jpg"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/reactGitPage/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/reactGitPage/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/reactGitPage/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/reactGitPage/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "eb6be414b75c412c13eeee2d2e8390cb",
+    "url": "/reactGitPage/static/media/react-logo.eb6be414.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/reactGitPage/precache-manifest.63c1cff18dfdb2cf4e83b73d2a93b5c6.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactGitPage/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
